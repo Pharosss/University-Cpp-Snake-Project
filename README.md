7a8ffa5f93049e9393232b83ed45dbd57117e16e
Pharosss <matkubiak888@gmail.com>
2022-12-25 20:01:34 +0100

    removed test files

--- README.md
+++ README.md
@@ -2,4 +2,4 @@
 
 A snake game made for Lodz University of Technology OOP Project. Made in c++ with ncurses and boost libraries.
 
-Linux onlyj
+Linux only
--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,13 +1,5 @@
 #include <iostream>
-#include "functions.h"
-#include "Student.h"
-
-using namespace std;
 
 int main() {
-    cout << "Hello, World!" << endl;
-    function();
-
-    Student student("Adam", "Smith");
-    return 0;
+    std::cout << "Hello, World!\n";
 }
