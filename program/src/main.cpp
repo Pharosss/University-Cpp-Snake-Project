85ccc007894e3b2a80c3d79c706eda866b63a868
Pharosss <matkubiak888@gmail.com>
2022-12-27 03:32:39 +0100

    Added Renderer demo

--- program/src/main.cpp
+++ program/src/main.cpp
@@ -1,5 +1,32 @@
 #include <iostream>
+#include "Renderer.h"
+
+void renderer_demo() {
+    Renderer r;
+    r.initialize();
+
+    r.write("Hello Ncurses!");
+    r.move_cursor(0, 2);
+    r.write("You can move to a discrete location");
+    r.move_relative(-1, 2);
+    r.write("Or to a relative one ...");
+    r.write("Always remember to refresh_screen() at the end!");
+    r.refresh_screen();
+    getchar();
+
+    r.clear_screen();
+    try {
+        r.move_cursor(-1, -2);
+    }
+    catch(std::exception e) {
+        r.write("Moving out of the bounds throws exception!");
+    }
+    r.refresh_screen();
+    getchar();
+
+    r.terminate();
+}
 
 int main() {
-    std::cout << "Hello, World!\n";
+    
 }
