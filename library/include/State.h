ff993b25639a9afc7372ec8a7613cb3e73cd5ab8
Pharosss <matkubiak888@gmail.com>
2022-12-27 20:13:10 +0100

    Added & implemented State class

---
+++ library/include/State.h
@@ -0,0 +1,28 @@
+#pragma once
+
+class State {
+    unsigned int score;
+    float speed_seconds;
+
+    bool m_is_finished;   // is the game over?
+    bool m_is_paused;
+    bool m_should_move;   // is automatically set to true every 1 second. After the move it resets
+
+public:
+    State();
+
+    float get_speed();
+    float set_speed(float seconds);
+
+    int get_score();
+    void increment_score();
+
+    bool is_finished();
+    void finish_game();
+
+    bool is_paused();
+    void set_pause(bool paused);
+
+    bool should_move();
+    void set_should_move(bool should);
+};
