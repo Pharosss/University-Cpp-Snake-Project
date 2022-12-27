baaa8323262834739e17921586b53c6d8f30223d
242218 <242218@edu.p.lodz.pl>
2022-12-27 12:03:25 +0100

    some changes in Game

--- program/src/Game.cpp
+++ program/src/Game.cpp
@@ -2,6 +2,7 @@
 // Created by gosia on 26/12/2022.
 //
 #include "Game.h"
+#include <iostream>
 Game::Game(unsigned int w, unsigned int h, float s){
     //tu w i h zainicjalizować
     speed_seconds=s;
@@ -17,6 +18,13 @@ int Game::get_score()
     return score;
 }
 
+bool Game::increment_score()
+{
+    //if()
+    //get_score()++;
+    return false;
+}
+
 bool Game::is_finished()
 {
     return is_finished;
@@ -24,8 +32,10 @@ bool Game::is_finished()
 
 void Game::end_game()
 {
-    if(is_finished())//?
-    exit(0);
+    if(is_finished){//tu chyba coś dodać trzeba
+        std::cout << "The game is over. You got "<< get_score()<< " scores.\n"; 
+        exit(0);
+    }
 }
 
 bool Game::is_paused()
@@ -37,5 +47,8 @@ void Game::set_pause(bool paused)//tu nie wiem
 {
     if(is_paused())
     paused=true;
-    else paused=false;
+    else {
+        paused=false;
+        should_move()==false;
+    }
 }
