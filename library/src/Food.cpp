e0657441298e98776c1ca270c10aec4c0f16d42e
Pharosss <matkubiak888@gmail.com>
2022-12-27 17:31:38 +0100

    implemented Food class (without update-render)

--- library/src/Food.cpp
+++ library/src/Food.cpp
@@ -1,5 +1,17 @@
-//
-// Created by gosia on 26/12/2022.
-//
 #include "Food.h"
-Food::Food(unsigned x, unsigned y):Entity(x, y){}
+
+Food::Food(unsigned x, unsigned y)
+ : Entity(x, y) {};
+
+
+void Food::update() {
+    // here place update
+};
+void Food::render(Renderer& renderer) {
+    // here place render
+};
+
+
+bool Food::is_food() {
+    return true;
+};
