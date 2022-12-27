08920c84d8f1a6841d93d36827e6910c9cf03cdb
Pharosss <matkubiak888@gmail.com>
2022-12-27 20:14:52 +0100

    fixed Body naming & smart_ptr issue

--- library/src/Body.cpp
+++ library/src/Body.cpp
@@ -1,8 +1,7 @@
 #include "Body.h"
 
 Body::Body(unsigned int x, unsigned int y)
- : Entity(x, y) {};
-
+    : Entity(x, y) {}
 
 void Body::update() {
     // here place update
@@ -13,19 +12,21 @@ void Body::render(Renderer& renderer) {
 
 
 void Body::attach(std::shared_ptr<Body> next) {
-    next = next;
+    this->next = next;
 };
 bool Body::is_tail() {
     return !next;
 };
 
 
-bool Body::is_at_recursive(unsigned int x, unsigned int y) {
-    return (get_x() == x && get_y() == y) || next->is_at_recursive(x, y);
+bool Body::is_at_recursive(unsigned int posx, unsigned int posy) {
+    return ((get_x() == posx && get_y() == posy) || next->is_at_recursive(posx, posy));
 };
-std::shared_ptr<Body> Body::get_tail_recursive() {
-    if(is_tail()) return std::make_shared<Body>(this);
-    else return next->get_tail_recursive();
+Body* Body::get_tail_recursive() {
+    if(is_tail())
+        return this;
+    else
+        return next->get_tail_recursive();
 };
 void Body::move_recursive(unsigned int x, unsigned int y) {
     auto new_x = get_x(),
