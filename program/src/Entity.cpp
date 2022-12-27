884076f6dfd57043cb9ee141a1976b77c0fce95e
242218 <242218@edu.p.lodz.pl>
2022-12-27 12:30:19 +0100

    in Entity

--- program/src/Entity.cpp
+++ program/src/Entity.cpp
@@ -6,3 +6,18 @@ Entity::Entity(unsigned int x, unsigned int y){
     this->x=x;
     this->y=y;
 }
+
+bool Entity::is_food()
+{
+    return false;
+}
+
+unsigned int Entity::get_x()
+{
+    return x;
+}
+
+unsigned int Entity::get_y()
+{
+    return y;
+}
