8ec3bd971117a898004240774939a264eb5a8bbb
242218 <242218@edu.p.lodz.pl>
2023-01-02 10:48:42 +0100

    some changes in Body test

--- library/test/BodyTest.cpp
+++ library/test/BodyTest.cpp
@@ -6,9 +6,9 @@ BOOST_AUTO_TEST_SUITE(BodyTest)
 BOOST_AUTO_TEST_CASE(Test_is_tail__attach) {
     //Body b(1, 1);
     std::shared_ptr<Body>next=std::make_shared<Body>(1, 1);
-    BOOST_CHECK_EQUAL(next.is_tail(), false);
+    BOOST_CHECK_EQUAL(next->is_tail(), false);
     next.attach(next);
-    BOOST_CHECK_EQUAL(next.is_tail(), true);
+    BOOST_CHECK_EQUAL(next->is_tail(), true);
 
     /* Body* b;
     BOOST_CHECK_EQUAL(b.is_at_recursive(b.get_x(), b.get_y()), true);
@@ -18,15 +18,14 @@ BOOST_AUTO_TEST_CASE(Test_is_tail__attach) {
     BOOST_CHECK_EQUAL(b.is_at_recursive(b.get_x(), b.get_y()), true);
     */}
 
-
    BOOST_AUTO_TEST_CASE(Test_recursive) {
     std::shared_ptr<Body>next;
 
-    next.move_recursive(1, 2);
-    BOOST_CHECK_EQUAL(next.is_at_recursive(next.get_x()-1, next.get_y()-2), true);
+    next->move_recursive(1, 2);
+    BOOST_CHECK_EQUAL(next->is_at_recursive(next->get_x()-1, next->get_y()-2), true);
 
     Body* body;    
-    BOOST_CHECK_EQUAL(body.get_tail_recursive(), body);
+    BOOST_CHECK_EQUAL(body->get_tail_recursive(), body);
     }
     
 BOOST_AUTO_TEST_SUITE_END()
