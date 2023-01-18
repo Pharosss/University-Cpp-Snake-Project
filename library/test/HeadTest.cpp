c5bd5daa89b7f94ca97f84c95c80b20aca3ff490
242218 <242218@edu.p.lodz.pl>
2023-01-18 08:29:58 +0100

    test attach in Headtest changed a bit

--- library/test/HeadTest.cpp
+++ library/test/HeadTest.cpp
@@ -4,14 +4,17 @@
 BOOST_AUTO_TEST_SUITE(HeadTest)
 
 BOOST_AUTO_TEST_CASE(Test_attach) {
-    Game* game;
-    std::shared_ptr<Body>head=std::make_shared<Body>(1, 1, game);
-    std::shared_ptr<Body>next=head.get_next();
-    head.attach();
-    //BOOST_CHECK_EQUAL(head.get_next(), );
+    Game *game;
+    Head head(1, 1, game);
+    std::shared_ptr<Body>a=head.get_next();
+    std::shared_ptr<Body>b=std::make_shared<Body>(2, 2, game);
+    head.attach(b);
+    //BOOST_CHECK_PREDICATE(a, head.attach(a));
+    BOOST_CHECK_EQUAL(a, head.attach(a));
+    BOOST_CHECK_PREDICATE(a, b);
     }
 
-    BOOST_AUTO_TEST_CASE(Test_attach) {
+    /* BOOST_AUTO_TEST_CASE(Test_attach) {
     Game* game;
     Head h;
     std::shared_ptr<Body>head;
@@ -24,6 +27,6 @@ BOOST_AUTO_TEST_CASE(Test_attach) {
     BOOST_CHECK_EQUAL(head, head2);
     h.attach(head);
     BOOST_CHECK_PREDICATE(head, head2);
-    }
+    } */
    
 BOOST_AUTO_TEST_SUITE_END()
