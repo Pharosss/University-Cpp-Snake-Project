a2b0cb557bf14154134cd972598b8d44408af8ca
Pharosss <matkubiak888@gmail.com>
2023-01-20 13:52:57 +0100

    is_at_ & move_ recursive() fixes

--- library/src/Body.cpp
+++ library/src/Body.cpp
@@ -2,7 +2,7 @@
 #include "Renderer.h"
 
 Body::Body(unsigned int x, unsigned int y)
-    : Entity(x, y) {}
+    : Entity(x, y), next(nullptr) {}
 
 void Body::update() {
     // here place update
@@ -21,7 +21,13 @@ bool Body::is_tail() {
 
 
 bool Body::is_at_recursive(unsigned int posx, unsigned int posy) {
-    return ((get_x() == posx && get_y() == posy) || next->is_at_recursive(posx, posy));
+    if (get_x() == posx && get_y() == posy)
+        return true;
+
+    if (!is_tail())
+        return next->is_at_recursive(posx, posy);
+
+    return false;
 };
 Body* Body::get_tail_recursive() {
     if(is_tail())
@@ -33,5 +39,6 @@ void Body::move_recursive(unsigned int x, unsigned int y) {
     auto new_x = get_x(),
         new_y = get_y();
     move(x, y);
-    next->move_recursive(new_x, new_y);
+    if (!is_tail())
+        next->move_recursive(new_x, new_y);
 };
