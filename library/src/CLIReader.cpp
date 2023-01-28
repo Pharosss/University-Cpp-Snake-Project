33018b6e1c65caf54ceb945a48b2e33ff816c7cf
Pharosss <matkubiak888@gmail.com>
2023-01-28 21:23:11 +0100

    Improved the CLIReader with stored lambdas

--- library/src/CLIReader.cpp
+++ library/src/CLIReader.cpp
@@ -1,35 +1,38 @@
 #include "CLIReader.h"
 #include <iostream>
 #include <string>
-
-// UTIL
-
-std::string find_flag_value(int argc, char *argv[], std::string flag) {
-    int index = -1;
-    std::string value = "";
-
-    for (int i = 1; i < argc; i++)
-        if (argv[i] == flag)
-            index = i;
-    
-    if (index > -1 && index + 1 < argc)
-        value = argv[index + 1];
-
-    return value;
-}
+#include <memory>
 
 void print_help_error() {
     std::cout<<"Please write -h or -help to access teh help page\n";
 }
 
+// CLIREADER
 
+CLIReader::CLIReader(unsigned int default_width, unsigned int default_height, float default_speed)
+  : should_start(false), board_w(default_width), board_h(default_height), speed_seconds(default_speed) {
 
+    // -c: clear highscore
+    argument_readers["-c"] = [](CLIReader* reader, std::string value){  
+        std::cout<<"Do you really want to clear the highscore? Write [yes/no].\n";
+        std::string response = "";
+        while (response != "yes" && response != "no")
+            std::cin >> response;
 
+        if (response == "yes") {
+            // clear the highscore
+            std::cout << "Highscore cleared!\n";
+        }
+        else if (response == "no")
+            std::cout<<"Highscore has not been cleared.\n";
+        
+        reader->should_start = false;
+        return -1;
+    };
 
-// OPTIONS
-
-void option_help_page() {
-    std::cout<<"Terminal Snake Game made by Mateusz Kubiak and Gosia Komorowska\n"
+    // -h / -help: help page
+    argument_readers["-h"] = argument_readers["-help"] = [](CLIReader* reader, std::string value) {
+        std::cout<<"Terminal Snake Game made by Mateusz Kubiak and Gosia Komorowska\n"
         << "@ Lodz University of Technology\n"
         << '\n'
         << "Controls:\n"
@@ -44,107 +47,99 @@ void option_help_page() {
         << "-H - show highscore. Does not run the game.\n"
         << "-c - clear highscore. Does not run the game.\n"
         << "-h (-help) - view this page.\n";
+        reader->should_start = false;
+        return -1;
+    };
+
+    argument_readers["-b"] = [](CLIReader* reader, std::string value){
+        if (value == "") {
+            std::cout<<"Error! Parameter -b cannot be empty!\n";
+            print_help_error();
+            reader->should_start = false;
+            return -1;
+        }
+        
+        int x_index = value.find('x');
+        if (x_index == -1) {
+            print_help_error();
+            reader->should_start = false;
+            return -1;
+        }
+
+        try {
+            int b_width = std::stoul(value.substr(0, x_index)),
+                b_height = std::stoul(value.substr(x_index + 1, value.size()));
+            
+            if (b_width < 1 || b_height < 1) {
+                std::cout<<"Error! Invalid board size argument. Board size has to be at least 1x1\n";
+                print_help_error();
+                reader->should_start = false;
+                return -1;
+            }
+
+            reader->board_w = b_width;
+            reader->board_h = b_height;
+        } catch(std::invalid_argument& e) {
+            std::cout<<"Error! Invalid board size argument. Use pattern UINTxUINT i.e. '-b 40x20'\n";
+            print_help_error();
+            reader->should_start = false;
+            return -1;
+        }
+    };
+
+    argument_readers["-d"] = [](CLIReader* reader, std::string value){
+        if (value == "") {
+            std::cout<<"Error! Difficulty argument cannot be empty. Use either s, small, m, medium, h or hard\n";
+            print_help_error();
+            reader->should_start = false;
+            return -1;
+        }
+        
+        if (value == "s" || value == "medium") {
+            reader->speed_seconds = .2f;
+        }
+        else if (value == "m" || value == "medium") {
+            reader->speed_seconds = .15f;
+        }
+        else if (value == "h" || value == "hard") {
+            reader->speed_seconds = .1f;
+        }
+        else {
+            std::cout<<"Error! Invalid difficulty argument. Use either s, small, m, medium, h or hard\n";
+            print_help_error();
+            reader->should_start = false;
+            return -1;
+        }
+
+        return 0;
+    };
+    
 }
-
-void option_clear_cache() {
-    std::cout<<"Do you really want to clear the highscore? Write [yes/no].\n";
-    std::string response = "";
-    while (response != "yes" && response != "no")
-        std::cin >> response;
-
-    if (response == "yes") {
-        // clear the highscore
-        std::cout << "Highscore cleared!\n";
-    }
-    else if (response == "no")
-        std::cout<<"Highscore has not been cleared.\n";
-}
-
-
-// CLIREADER
-
-CLIReader::CLIReader(unsigned int default_width, unsigned int default_height, float default_speed)
-  : should_start(false),
-    board_w(default_width),
-    board_h(default_height),
-    speed_seconds(default_speed) {}  //default parameters!
 
 void CLIReader::analyse_arguments(int argc, char *argv[]) {
 
-    // display parameters for debug
-    // std::cout << "argc: " << argc << '\n';
-    // for (size_t i = 0; i < argc; i++)
-    //     std::cout << "argv " << i << ": '" << argv[i] << "'\n";
-
-    // zero parameters - normal start
-    if (argc == 1) {
-        should_start = true;
-        return;
-    }
-
-    // -h / -help: help page
-    if (argc == 2 && (argv[1] == std::string("-h") || argv[1] == std::string("-help"))) {
-        option_help_page();
-        return;
-    }
-    
-    // -c: clear highscore
-    if (argc == 2 && argv[1] == std::string("-c")) {
-        option_clear_cache();
-        return;
-    }
-
-    // game parameters
-    if (argc >= 2) {
-        std::string b_param = find_flag_value(argc, argv, "-b"),
-                    d_param = find_flag_value(argc, argv, "-d"),
-                    s_param = find_flag_value(argc, argv, "-s");
-
-        std::cout<<"-b: " << b_param << '\n';
-        std::cout<<"-d: " << d_param << '\n';
-        std::cout<<"-s: " << s_param << '\n';
-
-        // b - board dimensions
-        if (b_param != "") {
-            int x_index = b_param.find('x');
-            if (x_index == -1) {
-                print_help_error();
-                return;
-            }
-
-            try {
-                int b_width = std::stoul(b_param.substr(0, x_index)),
-                    b_height = std::stoul(b_param.substr(x_index + 1, b_param.size()));
-                
-                if (b_width < 1 || b_height < 1) {
-                    std::cout<<"Error! Invalid board size argument. Board size has to be at least 1x1\n";
-                    print_help_error();
-                }
-
-                board_w = b_width;
-                board_h = b_height;
-            } catch(std::invalid_argument& e) {
-                std::cout<<"Error! Invalid board size argument. Use pattern UINTxUINT i.e. '-b 40x20'\n";
-                print_help_error();
-                return;
-            }
-        }
-
-        // Error - both -s and -d params
-        if (s_param != "" && d_param != "") {
-            std::cout<<"Error! Difficulty parameter -d and speed parameter -s cannot coexist!\n";
+    should_start = true;
+    for (unsigned int i = 1; i < argc; i++) {
+        if (argv[i][0] != '-')
+            continue;
+        
+        if (argument_readers.find(argv[i]) == argument_readers.end()) {
+            std::cout<<"Error! Unrecognized parameters.\n";
             print_help_error();
+            should_start = false;
             return;
         }
+        
+        // forbid values starting with '-'
+        std::string value = "";
+        if (i+1 < argc && argv[i+1][0] != '-')  
+            value = argv[i+1];
 
-        should_start = true;
-        return;
+        // readers can stop further execution
+        if (argument_readers.at(argv[i])(this, value) == -1) {
+            return;
+        }
     }
-
-
-
-    std::cout<<"Error! Unrecognized parameters.\n";
-    print_help_error();
 }
 
 
